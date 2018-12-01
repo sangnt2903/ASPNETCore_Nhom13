604cb1e783fa3b4affb046c3906371234e9e15af
ttthien111 <ttthien111@gmail.com>
2018-12-01 13:53:30 +0700

    CRUD appsetting

---
+++ ASPNETCore_Nhom13/ASPNETCore_Nhom13/Controllers/TheLoaisController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASPNETCore_Nhom13.Models;
+
+namespace ASPNETCore_Nhom13.Controllers
+{
+    public class TheLoaisController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public TheLoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TheLoais
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TheLoais.ToListAsync());
+        }
+
+        // GET: TheLoais/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var theLoai = await _context.TheLoais
+                .FirstOrDefaultAsync(m => m.MaTheLoai == id);
+            if (theLoai == null)
+            {
+                return NotFound();
+            }
+
+            return View(theLoai);
+        }
+
+        // GET: TheLoais/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TheLoais/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaTheLoai,TenTheLoai")] TheLoai theLoai)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(theLoai);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(theLoai);
+        }
+
+        // GET: TheLoais/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var theLoai = await _context.TheLoais.FindAsync(id);
+            if (theLoai == null)
+            {
+                return NotFound();
+            }
+            return View(theLoai);
+        }
+
+        // POST: TheLoais/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaTheLoai,TenTheLoai")] TheLoai theLoai)
+        {
+            if (id != theLoai.MaTheLoai)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(theLoai);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TheLoaiExists(theLoai.MaTheLoai))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(theLoai);
+        }
+
+        // GET: TheLoais/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var theLoai = await _context.TheLoais
+                .FirstOrDefaultAsync(m => m.MaTheLoai == id);
+            if (theLoai == null)
+            {
+                return NotFound();
+            }
+
+            return View(theLoai);
+        }
+
+        // POST: TheLoais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var theLoai = await _context.TheLoais.FindAsync(id);
+            _context.TheLoais.Remove(theLoai);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TheLoaiExists(int id)
+        {
+            return _context.TheLoais.Any(e => e.MaTheLoai == id);
+        }
+    }
+}
---
+++ ASPNETCore_Nhom13/ASPNETCore_Nhom13/Controllers/TinTucsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASPNETCore_Nhom13.Models;
+
+namespace ASPNETCore_Nhom13.Controllers
+{
+    public class TinTucsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public TinTucsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TinTucs
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.TinTucs.Include(t => t.NguoiDung).Include(t => t.TheLoai);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: TinTucs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tinTuc = await _context.TinTucs
+                .Include(t => t.NguoiDung)
+                .Include(t => t.TheLoai)
+                .FirstOrDefaultAsync(m => m.MaTin == id);
+            if (tinTuc == null)
+            {
+                return NotFound();
+            }
+
+            return View(tinTuc);
+        }
+
+        // GET: TinTucs/Create
+        public IActionResult Create()
+        {
+            ViewData["MaNguoiDung"] = new SelectList(_context.NguoiDungs, "MaNguoiDung", "HoTen");
+            ViewData["MaTheLoai"] = new SelectList(_context.TheLoais, "MaTheLoai", "TenTheLoai");
+            return View();
+        }
+
+        // POST: TinTucs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaTin,TieuDe,NoiDung,Hinh,NgayDang,MaTheLoai,MaNguoiDung")] TinTuc tinTuc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tinTuc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNguoiDung"] = new SelectList(_context.NguoiDungs, "MaNguoiDung", "HoTen", tinTuc.MaNguoiDung);
+            ViewData["MaTheLoai"] = new SelectList(_context.TheLoais, "MaTheLoai", "TenTheLoai", tinTuc.MaTheLoai);
+            return View(tinTuc);
+        }
+
+        // GET: TinTucs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tinTuc = await _context.TinTucs.FindAsync(id);
+            if (tinTuc == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaNguoiDung"] = new SelectList(_context.NguoiDungs, "MaNguoiDung", "HoTen", tinTuc.MaNguoiDung);
+            ViewData["MaTheLoai"] = new SelectList(_context.TheLoais, "MaTheLoai", "TenTheLoai", tinTuc.MaTheLoai);
+            return View(tinTuc);
+        }
+
+        // POST: TinTucs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaTin,TieuDe,NoiDung,Hinh,NgayDang,MaTheLoai,MaNguoiDung")] TinTuc tinTuc)
+        {
+            if (id != tinTuc.MaTin)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tinTuc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TinTucExists(tinTuc.MaTin))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNguoiDung"] = new SelectList(_context.NguoiDungs, "MaNguoiDung", "HoTen", tinTuc.MaNguoiDung);
+            ViewData["MaTheLoai"] = new SelectList(_context.TheLoais, "MaTheLoai", "TenTheLoai", tinTuc.MaTheLoai);
+            return View(tinTuc);
+        }
+
+        // GET: TinTucs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tinTuc = await _context.TinTucs
+                .Include(t => t.NguoiDung)
+                .Include(t => t.TheLoai)
+                .FirstOrDefaultAsync(m => m.MaTin == id);
+            if (tinTuc == null)
+            {
+                return NotFound();
+            }
+
+            return View(tinTuc);
+        }
+
+        // POST: TinTucs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tinTuc = await _context.TinTucs.FindAsync(id);
+            _context.TinTucs.Remove(tinTuc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TinTucExists(int id)
+        {
+            return _context.TinTucs.Any(e => e.MaTin == id);
+        }
+    }
+}
